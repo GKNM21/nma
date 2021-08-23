37875270d0bc340dd553d1d1e113abc27fbaa5b3
whynot <anil@panix.com>
2021-08-23 16:39:15 -0400

    RegistrationService

--- code/LiveCode/src/ttl/larku/app/RegistrationApp.java
+++ code/LiveCode/src/ttl/larku/app/RegistrationApp.java
@@ -5,15 +5,48 @@ import java.util.List;
 
 import ttl.larku.dao.StudentDAO;
 import ttl.larku.domain.Student;
+import ttl.larku.service.StudentService;
 
 /**
  * @author whynot
  */
 public class RegistrationApp {
 
+//	private StudentService studentService = new StudentService();
     public static void main(String[] args) {
-    	testDAO();
+    	//testDAO();
+    	RegistrationApp rapp = new RegistrationApp();
+    	rapp.postRequest();
+    	
+    	rapp.getRequest();
     }
+
+    public void postRequest() {
+    	StudentService studentService = new StudentService();
+        Student student = new Student("Josephine", LocalDate.of(1970, 2, 2), Student.Status.FULL_TIME);
+        int id1 = studentService.createStudent(student);
+
+        int id2 = studentService.createStudent("Bonaparte", LocalDate.of(1970, 2, 2), Student.Status.FULL_TIME);
+        
+        List<Student> students = studentService.getAllStudents();
+        for(Student s : students) {
+        	System.out.println(s);
+        }
+
+    }
+    
+    public List<Student> getRequest() {
+    	StudentService studentService = new StudentService();
+        List<Student> students = studentService.getAllStudents();
+        System.out.println("GetRequest");
+        for(Student s : students) {
+        	System.out.println(s);
+        }
+        
+        return students;
+    }
+
+
     
     public static void testStudent() {
         Student student = new Student("Josephine", LocalDate.of(1970, 2, 2), Student.Status.FULL_TIME);
@@ -23,7 +56,7 @@ public class RegistrationApp {
     }
     
     
-    public  static void testDAO() {
+    public static void testDAO() {
     	StudentDAO sd = new StudentDAO();
         Student student = new Student("Josephine", LocalDate.of(1970, 2, 2), Student.Status.FULL_TIME);
         sd.insert(student);
---
+++ code/LiveCode/src/ttl/larku/service/StudentService.java
@@ -0,0 +1,32 @@
+package ttl.larku.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import ttl.larku.dao.StudentDAO;
+import ttl.larku.domain.Student;
+import ttl.larku.domain.Student.Status;
+
+public class StudentService {
+
+	private StudentDAO studentDAO = new StudentDAO();
+	
+	public int createStudent(String name, LocalDate dob, Status status) {
+		Student s = new Student(name, dob, status);
+		return createStudent(s);
+	}
+	
+	public int createStudent(Student student) {
+		studentDAO.insert(student);
+		return student.getId();
+	}
+	
+	public Student getStudent(int id) {
+		return studentDAO.get(id);
+		
+	}
+
+	public List<Student> getAllStudents() {
+		return studentDAO.getAll();
+	}
+}
