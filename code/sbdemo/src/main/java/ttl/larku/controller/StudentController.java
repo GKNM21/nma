27e368ccec8210a9f00afe802382841dd5361326
whynot <anil@panix.com>
2021-08-30 16:32:21 -0400

    Spring Boot Controllers 2

--- code/sbdemo/src/main/java/ttl/larku/controller/StudentController.java
+++ code/sbdemo/src/main/java/ttl/larku/controller/StudentController.java
@@ -1,16 +1,42 @@
 package ttl.larku.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ttl.larku.domain.Student;
+import ttl.larku.service.StudentService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/sbdemo/student")
 public class StudentController {
+    private final StudentService studentService;
+
+//    @GetMapping
+//    public String doIt() {
+//        return "Will this really work?????";
+//    }
+    //Constructor injection
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @GetMapping
-    public String doIt() {
-        return "Will this really work?????";
+    public List<Student> getAllStudents() {
+       List<Student> students = studentService.getAllStudents();
+       return students;
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable("id") int id) {
+        Student student = studentService.getStudent(id);
+        return student;
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        Student newStudent = studentService.createStudent(student);
+
+        return newStudent;
     }
 }
